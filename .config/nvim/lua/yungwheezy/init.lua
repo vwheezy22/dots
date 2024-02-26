34f228e9f265384ddf4a2bdaa7faebbe85d04a8d
Vincent Whizin <vincent@wheezy.ftp.sh>
2024-02-26 10:40:22 -0800

    [WIP] Neovim lua migration

--- .config/nvim/lua/yungwheezy/init.lua
+++
@@ -1,6 +0,0 @@
-require("yungwheezy.lsp")
-require("yungwheezy.treesitter")
-require("yungwheezy.compe")
-require("yungwheezy.telescope")
-require("yungwheezy.hardline")
-require("yungwheezy.utils")
