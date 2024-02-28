196ee244739e41bc47b3b337b5190464effcda3a
Vincent Whizin <vincent@wheezy.ftp.sh>
2024-02-28 01:35:57 -0800

    Lots of stuff that I will likely not document later
    - Lazy lock
    - Fix autocmd to have camel case in theme with lua
    - Move plugin structure so lazy isn't loaded twice (avoids thrown
      warning)
    - Comment and lualine plugin, whetting my appetite

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,28 +1,28 @@
-local user_cmds = vim.api.nvim_create_augroup('user_cmds', { clear = true })
+local userCmds = vim.api.nvim_create_augroup('userCmds', { clear = true })
 vim.api.nvim_create_autocmd({ 'FileType' }, {
     desc = 'Spellcheck fix',
     pattern = {'tex', 'latex', 'markdown'},
     command = 'setlocal spell spelllang=en_us',
-    group = user_cmds,
+    group = userCmds,
 })
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
     desc = 'Remove trailing whitespace on save',
     pattern = {'*'},
     command = [[%s/\s\+$//e]],
-    group = user_cmds,
+    group = userCmds,
 })
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     desc = 'Annoying comment continuations',
     pattern = {'*'},
     command = 'setlocal formatoptions-=o formatoptions-=r',
-    group = user_cmds,
+    group = userCmds,
 })
 --[[
 vim.api.nvim_create_autocmd({ 'CursorHold' }, {
     desc = 'Show diagnostic popup on cursor hold',
     pattern = {'*'},
     callback = function() vim.lsp.diagnostic.show_line_diagnostics() end,
-    group = user_cmds,
+    group = userCmds,
 })
 --]]
 
